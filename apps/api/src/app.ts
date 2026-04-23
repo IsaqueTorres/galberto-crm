41fee58a1150bbd5763bc3db4d4a96dc56582061
Isaque Torres <torresisaque@gmail.com>
2026-04-23 12:13:37 -0300

    Criado integracao com PostgreSQL

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { sql } from "drizzle-orm";
+import { db } from "./db";
 
 export const app = new Hono();
 
@@ -7,4 +9,25 @@ app.get("/", (c) => {
     name: "Galberto CRM API",
     status: "ok"
   });
+});
+
+app.get("/health", async (c) => {
+  try {
+    const result = await db.execute(sql`select 1 as test`);
+    return c.json({
+      status: "ok",
+      database: "connected",
+      result,
+    });
+  } catch (error) {
+    console.error(error);
+    return c.json(
+      {
+        status: "error",
+        database: "disconnected",
+      },
+      500
+    );
+  };
+
 });
